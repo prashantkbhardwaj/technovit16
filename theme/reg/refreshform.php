14429558cb76a0baab8e141adc562da0bc5d5e82
aman_saha <aman97ram@gmail.com>
2016-08-05 02:40:44 +0530

    removed mail to check timeout

--- theme/reg/refreshform.php
+++ theme/reg/refreshform.php
@@ -46,32 +46,6 @@
 	}
 
 	else {
-
-		require 'PHPMailer-master/PHPMailerAutoload.php';
- 
-		$mail = new PHPMailer;
-		 
-		$mail->isSMTP();                                      
-		$mail->Host = 'smtp.gmail.com';                       
-		$mail->SMTPAuth = true;                               
-		$mail->Username = 'vibrancechennai@gmail.com';                   
-		$mail->Password = 'NayaWala';               
-		$mail->SMTPSecure = 'tls';                            
-		$mail->Port = 587;                                    
-		$mail->setFrom('vibrancechennai@gmail.com', 'Vibrance Registrations Team');
-		$mail->addAddress("$email");       
-		$mail->WordWrap = 50; 
-		$mail->isHTML(true);                                  
-		 
-		$mail->Subject = 'Vibrance event registration.';
-		$mail->Body    = 'You have successfully registered for <b>'.ucfirst($event_part[0]).'</b> in Vibrance16. Your E registration slip will be mailed and your participation will only be confirmed when you pay <b>Rs.'.$price.'</b> at our payment desks in VIT.'.'<br>'.' Regards, Team Vibrance. ';
-		$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';	
-
-		if(!$mail->send()) {
-		   echo 'Message could not be sent.';
-		   echo 'Mailer Error: ' . $mail->ErrorInfo;
-		   exit;
-		}
 		
 		
 		$query = "INSERT INTO {$event} (name, email, college, regno, phno, altphno, parti, combo, price)";
