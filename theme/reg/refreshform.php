8a4f0b02075b084a0886d00192137f6d9470b242
aman_saha <aman97ram@gmail.com>
2016-08-04 23:18:06 +0530

    mail test

--- theme/reg/refreshform.php
+++ theme/reg/refreshform.php
@@ -43,7 +43,6 @@
 	if ($check['email']== $email) {
 		echo "You have already registered for this event. ";
 	} 
-	
 	else {
 
 		require 'PHPMailer-master/PHPMailerAutoload.php';
@@ -70,12 +69,12 @@
 		   echo 'Message could not be sent.';
 		   echo 'Mailer Error: ' . $mail->ErrorInfo;
 		   exit;
-		}	
+		}
 		
 		
 		$query = "INSERT INTO {$event} (name, email, college, regno, phno, altphno, parti, combo, price)";
 		$query .= " VALUES ('{$name}', '{$email}', '{$college}', '{$regno}', '{$phno}', '{$altphno}', {$parti}, '{$combo}', {$price})";
-		$result = mysqli_query($conn, $query);
+		$result = mysqli_query($conn, $query);	
 
 	    if ($result) {
 	      	echo"You have succesfully registered for Vibrance16. Please check your email for the details. Your registraion will only be confirmed after you make the payment at our registration desk in VIT.";		
